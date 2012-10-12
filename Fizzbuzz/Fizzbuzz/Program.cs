d7aeb5c9f8aa5cd2e580b006955b7468773df52d
Andrew Brown <XolotlRise@gmail.com>
2012-10-12 17:57:06 -0500

    Initial load of fizzbuzz

---
+++ Fizzbuzz/Fizzbuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Xolotl
+ * Date: 10/12/2012
+ * Time: 5:49 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Fizzbuzz
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
